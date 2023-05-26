d5bf8550c06f43348b310898245d3246599b89c8
Prompiriya1 <132637448+Prompiriya1@users.noreply.github.com>
2023-05-26 05:06:16 +0000

    Initial commit

---
+++ student.h
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class student{
+private:
+  long id;
+  string name;
+  int age;
+
+public:
+ void set_name(string);
+ void print_name();
+  void set_age(int);
+  int get_age();
+};
+
+void student :: set_name(string n){
+name=n;}
+void student :: print_name(){
+  cout<< "name: " <<name<<endl;
+}
+
+void student :: set_age(int n){
+ age=n;
+}
+int student :: get_age()
+  {
+  return age;
+}
